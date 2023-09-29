33a35bd458547f1fcb78f9188ba475b99a128f21
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2023-09-29 17:55:26 +0200

    todo state

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react'
 import {Title} from './components/Title/Title'
 import {ToDo} from './components/Todo/Todo'
 import {ToDoInput} from './components/TodoInput/ToDoInput'
@@ -6,17 +7,40 @@ import {ToDoList} from './components/TodoList/ToDoList'
 
 
 const App = () => {
+
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: "Watch the next Marvel Movie",
+      completed: false,
+    },
+    {
+      id: 2,
+      title: "Record the next Video",
+      completed: false,
+    },
+    {
+      id: 3,
+      title: "Wash the dishes",
+      completed: false,
+    },
+    {
+      id: 4,
+      title: "Study 2 hours",
+      completed: false,
+    },
+  ])
+
+  
+
+
   return (
     <div className="bg-gray-900 min-h-screen h-full w-full font-inter text-gray-100 flex items-center justify-center py-20 px-5 ">
       <div className='container flex flex-col max-w-xl'>
         <Title/>
         <ToDoInput />
-        <ToDoList>
-          <ToDo />
-          <ToDo />
-          <ToDo />
-          <ToDo />
-        </ToDoList>
+        <ToDoList todos={todos} />
+         
       </div>
     </div>
   )
--- todo-app/src/components/TodoFilters/ToDoFilters.jsx
+++ todo-app/src/components/TodoFilters/ToDoFilters.jsx
@@ -19,4 +19,4 @@ const ToDoFilters = () => {
   )
 }
  
-export default ToDoFilters
+export { ToDoFilters }
