5ea629d8e1ecfcf94de32597e3e4e5d9202c99af
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2023-09-29 17:37:14 +0200

    filters update and clear tasks button

--- todo-app/src/components/TodoFilters/ToDoFilters.jsx
+++ todo-app/src/components/TodoFilters/ToDoFilters.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
-import { FilterContainer, ItemsLeft } from "./ToDoFilters.components"
+import { FilterButton, FilterButtonContainer, FilterContainer, ItemsLeft } from "./ToDoFilters.components"
 
 
 const ToDoFilters = () => {
   return (
     <FilterContainer>
         <ItemsLeft />
+        <FilterButtonContainer>
+          <FilterButton action={() => {}} filter="All" />
+          <FilterButton action={() => {}} filter="Active" />
+          <FilterButton action={() => {}} filter="Completed" />
+        </FilterButtonContainer>
+
+        <button className='text-gray-400 hover:text-white cursor-pointer transition-all duration-300 ease-in'>
+          Clear Completed
+        </button>
     </FilterContainer>
   )
 }
