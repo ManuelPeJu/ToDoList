180359ad7f9ac274abef9b7ca12db84831aca0c1
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2023-09-29 18:07:46 +0200

    add task

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -31,14 +31,26 @@ const App = () => {
     },
   ])
 
-  
 
+  const addTodo = (title) => {
+    const lastId = todos.length > 0 ? todos[todos.length - 1].id : 1;
+
+    const newTodo = {
+      id: lastId + 1,
+      title,
+      completed: false
+    }
+
+    const todoList = [...todos]
+    todoList.push(newTodo);
+    setTodos(todoList);
+  }
 
   return (
     <div className="bg-gray-900 min-h-screen h-full w-full font-inter text-gray-100 flex items-center justify-center py-20 px-5 ">
       <div className='container flex flex-col max-w-xl'>
         <Title/>
-        <ToDoInput />
+        <ToDoInput addTodo={addTodo} />
         <ToDoList todos={todos} />
          
       </div>
